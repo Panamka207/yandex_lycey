3846a4f8e1e879d5762b933c50f66c6f67a8ea51
reggina-hodyreva <hrr23@gmail.com>
2026-03-12 18:17:30 +0500

    Задание: Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,15 @@ def training(prof):
     return render_template('training.html', prof=prof.lower())
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    return render_template('list_prof.html', list=list, professions=professions)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
