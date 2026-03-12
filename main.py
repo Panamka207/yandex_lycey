fce6bf3d17d91f68d91245610c65ff00b295980c
reggina-hodyreva <hrr23@gmail.com>
2026-03-12 18:23:28 +0500

    Задание: Автоматический ответ

--- main.py
+++ main.py
@@ -24,5 +24,20 @@ def list_prof(list):
     return render_template('list_prof.html', list=list, professions=professions)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    profil = {}
+    profil['title'] = 'Анкета'
+    profil['surname'] = 'Watny'
+    profil['name'] = 'Mark'
+    profil['education'] = 'выше среднего'
+    profil['profession'] = 'штурман марсохода'
+    profil['sex'] = 'male'
+    profil['motivation'] = 'Всегда мечтал застрять на Марсе!'
+    profil['ready'] = 'True'
+    return render_template('auto_answer.html', **profil)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
